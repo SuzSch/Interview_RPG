094b27af8c9f16a919dba1b1406fe13073ca60f7
Suzanne <suzanneschuber@gmail.com>
2023-08-30 15:39:18 -0700

    Finish up RPG functions. WIP: set up Index and UI logic Co-authored By: Jason Church

--- src/InteviewRPG.js
+++ src/InteviewRPG.js
@@ -1,5 +1,4 @@
-
-const characterCreator = (intialState) => {
+ const characterCreator = (intialState) => {
     let currentState = intialState;
     return (stateChangeFunction = state => state) => {
       const newState = stateChangeFunction(currentState);
@@ -8,12 +7,38 @@ const characterCreator = (intialState) => {
     }
   };
 
-const Barbarian = characterCreator({HP: 100, Charisma: 3, Rest: 9, Knowledge: 3, Preparedness: 6, Intimidation: 9, Focus: 6});
+export const Barbarian = characterCreator({HP: 100, Charisma: 3, Rest: 9, Knowledge: 3, Preparedness: 6, Intimidation: 9, Focus: 6});
 
-const Wizard = characterCreator({HP: 100, Charisma: 6, Rest: 3, Knowledge: 9, Preparedness: 6, Intimidation: 6, Focus: 9});
+export const Wizard = characterCreator({HP: 100, Charisma: 6, Rest: 3, Knowledge: 9, Preparedness: 6, Intimidation: 6, Focus: 9});
 
-const Bard = characterCreator({HP: 100, Charisma: 9, Rest: 6, Knowledge: 3, Preparedness: 9, Intimdation: 3, Focus: 6});
+export const Bard = characterCreator({HP: 100, Charisma: 9, Rest: 6, Knowledge: 3, Preparedness: 9, Intimidation: 3, Focus: 6});
 
-const Monk = characterCreator({HP: 100, Charisma: 3, Rest: 6, Knowledge: 6, Preparedness: 9, Intimdation: 3, Focus: 9});
+export const Monk = characterCreator({HP: 100, Charisma: 3, Rest: 6, Knowledge: 6, Preparedness: 9, Intimidation: 3, Focus: 9});
 
-const Rogue = characterCreator({HP: 100, Charisma: 9, Rest: 3, Knowledge: 3, Preparedness: 6, Intimdation: 6, Focus: 9 }); 
+export const Rogue = characterCreator({HP: 100, Charisma: 9, Rest: 3, Knowledge: 3, Preparedness: 6, Intimidation: 6, Focus: 9 }); 
+
+export const BossMonster = characterCreator({HP:100});
+
+const changeStats = (prop) => {
+    return (value) => {
+        return (state) => ({
+            ...state,
+            [prop] : (state[prop]) + value
+        })
+    }
+}
+// Change skill stats
+export const drinkCoffee = changeStats("Rest")(2);
+export const studyMaterial = changeStats("Knowledge")(2);
+export const meditate = changeStats("Focus")(2);
+export const pumpIron = changeStats("Intimidation")(2);
+export const takeShower = changeStats("Charisma")(2);
+export const researchCompany = changeStats("Preparedness")(2);
+
+//Change HP
+
+export const critHPloss = changeStats("HP")(-20);
+export const veryHPloss = changeStats("HP")(-15);
+export const normalHPloss = changeStats("HP")(-10);
+export const okHPloss = changeStats("HP")(-5);
+
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './css/styles.css';
+
+// import { Barbarian, Wizard, Bard, Monk, Rogue, BossMonster, drinkCoffee, studyMaterial, meditate, pumpIron, takeShower, researchCompany, critHPloss, veryHPloss, normalHPloss, okHPloss} from "./InteviewRPG";
+
+
